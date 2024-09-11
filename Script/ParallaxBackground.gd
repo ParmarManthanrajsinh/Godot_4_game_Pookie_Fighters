18435498099fb9a6add4452df65020217b306532
ParmarManthanrajsinh <manthanrajsinh@gmail.com>
2024-09-11 23:50:09 +0530

    updated the player selection code

--- Script/ParallaxBackground.gd
+++ Script/ParallaxBackground.gd
@@ -1,23 +1,22 @@
 extends ParallaxBackground
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta) -> void:
-	scroll_offset.x -= delta * 50
+	scroll_offset.x -= delta * 50 #update the background of the scene
 
 
-func _on_play_pressed():
+func _on_play_pressed() -> void:
+	# change scene to player selection scene
 	get_tree().change_scene_to_file("res://scene/player_selection.tscn")
 
 
-func _on_controlls_pressed():
+func _on_controlls_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_about_pressed():
+func _on_about_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_exit_pressed():
-	get_tree().quit()
+func _on_exit_pressed() -> void:
+	get_tree().quit() # quit game
 
--- Script/Player_selection.gd
+++ Script/Player_selection.gd
@@ -4,7 +4,8 @@ var warrier := preload("res://scene/warrier.tscn")
 var speedster := preload("res://scene/speedster.tscn")
 var tank := preload("res://scene/tank.tscn")
 
-func _ready():
+func _ready() -> void:
+	# Initialize starting selected character
 	var warr1 = warrier.instantiate()
 	get_node("Player1/Panel").add_child(warr1)
 	warr1.play()
@@ -12,7 +13,14 @@ func _ready():
 	get_node("Player2/Panel").add_child(warr2)
 	warr2.play()
 
-func _on_player_1_type_item_selected(index):
+func _on_player_1_type_item_selected(index) -> void:
+	
+	# Removing the previously selected character
+	for n in get_node("Player1/Panel").get_children():
+		get_node("Player1/Panel").remove_child(n)
+		n.queue_free()
+	
+	# Adding the selected character
 	match index:
 		0:
 			var warr = warrier.instantiate()
@@ -28,7 +36,14 @@ func _on_player_1_type_item_selected(index):
 			ta.play()
 
 
-func _on_player_2_type_item_selected(index):
+func _on_player_2_type_item_selected(index) -> void:
+	
+	# Removing the previously selected character
+	for n in get_node("Player2/Panel").get_children():
+		get_node("Player2/Panel").remove_child(n)
+		n.queue_free()
+	
+	# Adding the selected character
 	match index:
 		0:
 			var warr = warrier.instantiate()
@@ -42,3 +57,7 @@ func _on_player_2_type_item_selected(index):
 			var ta = tank.instantiate()
 			get_node("Player2/Panel").add_child(ta)
 			ta.play()
+
+func _on_back_pressed() -> void:
+	# Send back to the main scene
+	get_tree().change_scene_to_file("res://scene/main_menu.tscn")

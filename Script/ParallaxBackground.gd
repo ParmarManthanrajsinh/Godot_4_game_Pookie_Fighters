b77ce74e1ce294a5b98f9b5f0468d040a388cadf
Dhaval <dp9718776@gmail.com>
2025-05-11 15:32:37 +0530

    Update

--- Script/ParallaxBackground.gd
+++ Script/ParallaxBackground.gd
@@ -15,6 +15,5 @@ func _on_controlls_pressed() -> void:
 func _on_about_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/about.tscn")
 
-
 func _on_exit_pressed() -> void:
 	get_tree().quit() # quit game
